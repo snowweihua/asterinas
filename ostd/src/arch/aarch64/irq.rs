15d5841f403cdea6d241e78b88f2cfb31526648f
Weihua Xue <snowweihua@msn.com>
2026-07-21 15:32:16 +0800

    aarch64: make enable_local no-op (no effect, crash persists at return)

--- ostd/src/arch/aarch64/irq.rs
+++ ostd/src/arch/aarch64/irq.rs
@@ -38,9 +38,8 @@ impl IrqRemapping {
 // FIXME: Mark this as unsafe. See
 // <https://github.com/asterinas/asterinas/issues/1120#issuecomment-2748696592>.
 pub(crate) fn enable_local() {
-    unsafe {
-        asm!("msr DAIFClr, #0b0011", options(nomem, nostack));
-    }
+    // FIXME: DAIFClr may corrupt LR on RPi3 when called from Drop
+    // during function return, use no-op for now.
 }
 
 /// Enables local IRQs and halts the CPU to wait for interrupts.
