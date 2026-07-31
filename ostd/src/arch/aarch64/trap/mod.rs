9135b369147e20100c3dd16c539c6a8c926554e8
Weihua Xue <snowweihua@msn.com>
2026-07-31 12:25:43 +0800

    aarch64/rpi3: full EL1 sync exception diagnostics working - ESR and ELR printed

--- ostd/src/arch/aarch64/trap/mod.rs
+++ ostd/src/arch/aarch64/trap/mod.rs
@@ -119,6 +119,8 @@ extern "C" fn sync_exception_current(f: &mut TrapFrame) {
     put_crlf!();
     put_str!(b" ESR=");
     put_hex!(f.esr_el1);
+    put_str!(b" ELR=");
+    put_hex!(f.elr_el1);
     put_crlf!();
     put_str!(b"### EL1 HALT ###");
     put_crlf!();
